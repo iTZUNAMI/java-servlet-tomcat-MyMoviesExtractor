275d1f66cb84aac7605cef50b67d7dd2ccd3dc71
iTZUNAMI <th3ozz@gmail.com>
2020-01-28 16:45:05 +0100

    added web interface

---
+++ www/cerca-cinema/img/list_news.js
@@ -0,0 +1,7 @@
+ /*Example message arrays for the two demo scrollers*/
+
+var pausecontent2=new Array()
+
+pausecontent2[0]='<a href="http://filmup.leonardo.it/curiosita/20120420a.shtml">In sala il tributo di Allen a Roma</a>'
+pausecontent2[1]='<a href="http://filmup.leonardo.it/unaspianonbasta.htm">RECENSIONI: Una spia non basta</a>'
+pausecontent2[2]='<a href="http://filmup.leonardo.it/curiosita/20120419a.shtml">Jason Segel e Emily Blunt inaugurano il Tribeca Film Festival</a>'
